e45b6d33f15ad7167a50b17b7a2035416e035d86
DimitrusB <dimitrusboss@mail.ru>
2023-04-28 23:25:56 +0300

    DOM12

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ const nameInputElement = document.getElementById("name-input");
 const commentInputElement = document.getElementById("comment-input");
 const listElement = document.getElementById("list"); 
 const inputs = document.querySelectorAll('#name-input, #comment-input'); //для отчистки формы ввода после отправки данных
+
+const comments = [
+  {
+   date: "12.02.22 12:18",
+   name: "Глеб Фокин",
+   comment: "Это будет первый комментарий на этой странице",
+   like: "3"
+  },
+  {
+    date: "13.02.22 19:22",
+    name: "Варвара Н.",
+    comment: "Мне нравится как оформлена эта страница! ❤",
+    like: "75"
+  }
+];
+
+
+
 const dd = new Date();
 let day = dd.getDate();
 let mon = dd.getMonth()+1;
@@ -15,12 +33,49 @@ if (day <10){
 }
 const year = dd.getFullYear() - 2000;
 let time = new Date().toLocaleTimeString().slice(0,-3);
+
+const renderComments = () =>{
+  const commentsHtml = comments.map((comm, index) =>{
+return     `<li class="comment">
+<div class="comment-header">
+  <div>
+    ${comm.name}
+  </div>
+  <div>${day}.${mon}.${year} ${time}</div>
+</div>
+<div  class="comment-body">
+  <div class="comment-text">
+    ${comm.comment}
+  </div>
+</div>
+<div class="comment-footer">
+  <div class="likes">
+    <span class="likes-counter">${comm.like}</span>
+    <button class="like-button"></button>
+  </div>
+</div>
+</li>`;
+  }).join('');
+  listElement.innerHTML = commentsHtml;
+};
+
+renderComments();
+
 buttonElement.addEventListener ("keydown" && "click",  () => {
 
-if (nameInputElement.value === "" || commentInputElement.value === ""){
-buttonElement.disable = "true";
-return;
-};
+  if (nameInputElement.value === "" || commentInputElement.value === ""){
+  buttonElement.disable = "true";
+  return;
+  }
+  comments.push({
+    name: nameInputElement.value,
+    date: dd.value,
+    comment: commentInputElement.value,
+    
+  });
+renderComments();
+  };
+
 const oldListHtml = listElement.innerHTML;
   listElement.innerHTML =
     oldListHtml +
